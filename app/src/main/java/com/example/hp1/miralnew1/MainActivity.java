aa1a0743c8087cfacfccdb0390f67561261737ac
faisalsadi <fesalsadi@gmail.com>
2017-09-14 12:41:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hp1/miralnew1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hp1.miralnew1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
