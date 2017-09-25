78775daf53ad4ccd2fe0f2b1e162f78c0e4f74d5
miralsobuh <miralsobo7@gmail.com>
2017-09-25 13:27:37 +0300

    ss

--- app/src/main/java/com/example/hp1/miralnew1/MainActivity.java
+++ app/src/main/java/com/example/hp1/miralnew1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.hp1.miralnew1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(position==0){
+            Intent i=new Intent(this, Main2Activity.class);
+            startActivity(i);
+        }
     }
 }
