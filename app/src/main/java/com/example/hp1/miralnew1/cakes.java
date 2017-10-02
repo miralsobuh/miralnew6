afb1896c12e33cec6015b2349b474a2bb93aae5a
miralsobuh <miralsobo7@gmail.com>
2017-10-02 13:13:04 +0300

    aa

--- app/src/main/java/com/example/hp1/miralnew1/cakes.java
+++ app/src/main/java/com/example/hp1/miralnew1/cakes.java
@@ -2,12 +2,44 @@ package com.example.hp1.miralnew1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class cakes extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class cakes extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    ListView lvcakes;//3rfna 3list view
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrcakes=new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cakes);
+
+        lvcakes=(ListView) findViewById(R.id.lvMyCakes);
+
+        arrcakes.add("Chees_cake");
+        arrcakes.add("Orange_cake");
+        arrcakes.add("Chocolate_cake");
+
+        adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrcakes);
+        lvcakes.setAdapter(adapter);
+        lvcakes.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
     }
 }
--- app/src/main/java/com/example/hp1/miralnew1/milkshakes.java
+++ app/src/main/java/com/example/hp1/miralnew1/milkshakes.java
@@ -2,12 +2,19 @@ package com.example.hp1.miralnew1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class milkshakes extends AppCompatActivity {
+public class milkshakes extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_milkshakes);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
