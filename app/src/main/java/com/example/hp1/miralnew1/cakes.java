cf1e6d1be48abf22288402e43c64f96363e5073e
miralsobuh <miralsobo7@gmail.com>
2017-10-09 13:30:48 +0300

    aa

--- app/src/main/java/com/example/hp1/miralnew1/cakes.java
+++ app/src/main/java/com/example/hp1/miralnew1/cakes.java
@@ -1,5 +1,6 @@
 package com.example.hp1.miralnew1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class cakes extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class cakes extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView lvcakes;//3rfna 3list view
     ArrayAdapter<String> adapter;
@@ -28,18 +29,20 @@ public class cakes extends AppCompatActivity implements View.OnClickListener, Ad
 
         adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrcakes);
         lvcakes.setAdapter(adapter);
-        lvcakes.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
+        lvcakes.setOnItemClickListener(this);
 
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        if(position == 0 ){
+            Intent i = new Intent(this, milkshakes.class);
+            startActivity(i);
+        }
+        if(position == 1){
+            Intent i = new Intent(this, cakes.class);
+            startActivity(i);
+        }
 
     }
 }
